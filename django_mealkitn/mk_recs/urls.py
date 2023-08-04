2aeb583c20ccdeae6b84f342396ae38efed504e1
Dajeong Heo <hdj1196@gmail.com>
2023-08-04 22:22:20 +0900

    mk_recs urls 충돌 문제 해결
    - home urls 주석처리. 기본 url과 충돌으로 에러.

--- django_mealkitn/mk_recs/urls.py
+++ django_mealkitn/mk_recs/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'mk_recs'
 urlpatterns = [
-    path('index/', views.home, name='index'),
+    # path('index/', views.home, name='index'),
     path('mealkit/', views.mk_recs_result, name='mk_recs_result'),
+    path('recipe/', views.rcp_recs_result, name='recipe'),
 ]
